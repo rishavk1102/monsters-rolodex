a91c06e5bd474858111c3250f1dace72a6e883df
rishavk1102 <rishavk1102@gmail.com>
2020-08-20 03:14:05 +0530

    Updates

---
+++ precache-manifest.e033e674018afabc6110e02d16ea8bae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7b32f403798f972855653adbf794e9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b1d3b44057ca01d70892",
+    "url": "/monsters-rolodex/static/css/main.97522895.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d3b44057ca01d70892",
+    "url": "/monsters-rolodex/static/js/main.f0140199.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
